4f9c53b0ece7160a94119e22f6bb09627c9b1f0f
CodingRuo <r.schober@outlook.com>
2023-05-14 15:40:36 +0200

    config: setting up ground game engine

--- src/main.js
+++ src/main.js
@@ -1,18 +1,27 @@
 
 import { Level } from "./level";
+import { Pointer } from "./pointer";
+import { Settings } from "./settings";
 
 class Game {
   constructor() {
     this.level = new Level()
     this.previoustime = Date.now();
 
+    Pointer.init();
+
     this.run();
   }
 
 
-  run() {
+  run = () => {
     let newtime = Date.now();
+    Settings.dt = (newtime - this.previoustime) / 1000;
+    this.previoustime = newtime;
 
+    this.level.run();
+
+    requestAnimationFrame(this.run)
   }
 
 
